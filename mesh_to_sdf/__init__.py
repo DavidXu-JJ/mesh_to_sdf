a889199a3589aa85d635032775102cc779842e6c
davidxujw <davidxujw@gmail.com>
2024-11-10 21:01:34 -0800

    update udf call

--- mesh_to_sdf/__init__.py
+++ mesh_to_sdf/__init__.py
@@ -40,6 +40,24 @@ def mesh_to_sdf(mesh, query_points, surface_point_method='scan', sign_method='no
     else:
         raise ValueError('Unknown sign determination method: {:s}'.format(sign_method))
 
+def mesh_to_udf(mesh, query_points, surface_point_method='scan', sign_method='normal', bounding_radius=None, scan_count=100, scan_resolution=400, sample_point_count=10000000, normal_sample_count=11):
+    if not isinstance(query_points, np.ndarray):
+        raise TypeError('query_points must be a numpy array.')
+    if len(query_points.shape) != 2 or query_points.shape[1] != 3:
+        raise ValueError('query_points must be of shape N ✕ 3.')
+    
+    if surface_point_method == 'sample' and sign_method == 'depth':
+        print("Incompatible methods for sampling points and determining sign, using sign_method='normal' instead.")
+        sign_method = 'normal'
+
+    point_cloud = get_surface_point_cloud(mesh, surface_point_method, bounding_radius, scan_count, scan_resolution, sample_point_count, calculate_normals=sign_method=='normal')
+
+    if sign_method == 'normal':
+        return point_cloud.get_udf_in_batches(query_points, use_depth_buffer=False)
+    elif sign_method == 'depth':
+        return point_cloud.get_udf_in_batches(query_points, use_depth_buffer=True, sample_count=sample_point_count)
+    else:
+        raise ValueError('Unknown sign determination method: {:s}'.format(sign_method))
 
 def mesh_to_voxels(mesh, voxel_resolution=64, surface_point_method='scan', sign_method='normal', scan_count=100, scan_resolution=400, sample_point_count=10000000, normal_sample_count=11, pad=False, check_result=False, return_gradients=False):
     mesh = scale_to_unit_cube(mesh)
