6f623e5d44ac4d63e0bc69566eab9e4eff78271a
davidxujw <davidxujw@gmail.com>
2024-11-10 21:09:41 -0800

    Fix: require input mesh path.

--- mesh_to_sdf/__init__.py
+++ mesh_to_sdf/__init__.py
@@ -70,7 +70,7 @@ def mesh_to_sdf(mesh, query_points, surface_point_method='scan', sign_method='no
     else:
         raise ValueError('Unknown sign determination method: {:s}'.format(sign_method))
 
-def mesh_to_udf(mesh, query_points, surface_point_method='scan', sign_method='normal', bounding_radius=None, scan_count=100, scan_resolution=400, sample_point_count=10000000, normal_sample_count=11):
+def mesh_to_udf(mesh_path, query_points, surface_point_method='scan', sign_method='normal', bounding_radius=None, scan_count=100, scan_resolution=400, sample_point_count=10000000, normal_sample_count=11):
     if not isinstance(query_points, np.ndarray):
         raise TypeError('query_points must be a numpy array.')
     if len(query_points.shape) != 2 or query_points.shape[1] != 3:
@@ -80,7 +80,7 @@ def mesh_to_udf(mesh, query_points, surface_point_method='scan', sign_method='no
         print("Incompatible methods for sampling points and determining sign, using sign_method='normal' instead.")
         sign_method = 'normal'
 
-    point_cloud = get_surface_point_cloud_fast(mesh, surface_point_method, bounding_radius, scan_count, scan_resolution, sample_point_count, calculate_normals=sign_method=='normal')
+    point_cloud = get_surface_point_cloud_fast(mesh_path, surface_point_method, bounding_radius, scan_count, scan_resolution, sample_point_count, calculate_normals=sign_method=='normal')
 
     if sign_method == 'normal':
         return point_cloud.get_udf_in_batches(query_points, use_depth_buffer=False)
